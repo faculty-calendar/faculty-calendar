da3d24170214efcfd701843340bc5d7ca367559a
droideronline <dinesh.suriya106@gmail.com>
2023-05-20 23:03:33 +0530

    Home: Redirect to calendar if user is aldready logged.
    Signed-off-by: droideronline <dinesh.suriya106@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
@@ -13,33 +12,44 @@ import "./App.css";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         setUser(user);
+        navigate("/calendar");
       } else {
         setUser(null);
       }
+      setLoading(false);
     });
 
     return () => unsubscribe();
-  }, []);
+  }, [navigate]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home name={user?.displayName} />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/calendar" element={<Calendar />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home name={user?.displayName} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/calendar" element={<Calendar />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
-
+export default function WrappedApp() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
